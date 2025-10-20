bea0061053047296109fa686f514935445325197
Sivatheevan <sivatheevan1224@gmail.com>
2025-10-20 20:15:44 +0530

    feat: Add favicon and update metadata for improved branding

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,6 +17,9 @@ const geistMono = localFont({
 export const metadata: Metadata = {
   title: "Sivatheevan Arulnathan",
   description: "This is my portfolio website",
+  icons: {
+    icon: '/favicon.png',
+  },
 };
 
 export default function RootLayout({
